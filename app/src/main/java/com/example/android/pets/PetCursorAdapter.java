1606b7aa9e7ba83b5773264997966c50a974ea47
Luis Perez <lperez.1092@gmail.com>
2016-11-11 14:14:25 -0430

    Muestra texto de "Raza desconocida" en mascotas sin raza

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,6 +67,10 @@ public class PetCursorAdapter extends CursorAdapter {
         String petName =  cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
 
+        // If breed is empty or null use "Unknown breed" default text
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
         // Set data on TextViews
         nameTextView.setText(petName);
         summaryTextView.setText(petBreed);

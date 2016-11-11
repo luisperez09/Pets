f6b5b135ff6d5612a1c37f5eb25e8f1565858d20
Luis Perez <lperez.1092@gmail.com>
2016-11-11 13:29:11 -0430

    Elimina mascota desde el menú del editor

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -257,7 +257,8 @@ public class EditorActivity extends AppCompatActivity implements
                 return true;
             // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
-                // Do nothing for now
+                // Pop up confirmation dialog for deletion
+                showDeleteConfirmationDialog();
                 return true;
             // Respond to a click on the "Up" arrow button in the app bar
             case android.R.id.home:
@@ -388,5 +389,53 @@ public class EditorActivity extends AppCompatActivity implements
         // Create and show the AlertDialog
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    /**
+     * Prompt the user to confirm that they want to delete this pet.
+     */
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the pet.
+                deletePet();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    /**
+     * Perform the deletion of the pet in the database.
+     */
+    private void deletePet() {
+        // Only perform the delete if there is an existing pet
+        if (mCurrentPetUri != null){
+            int rowsDeleted = getContentResolver().delete(mCurrentPetUri, null, null);
+
+            // Show toast message depending on delete outcome
+            if (rowsDeleted > 0) {
+                Toast.makeText(EditorActivity.this, getString(R.string.editor_delete_pet_successful), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(EditorActivity.this, getString(R.string.editor_delete_pet_failed), Toast.LENGTH_SHORT).show();
+            }
+        }
+        // Pet was deleted, close activity
+        finish();
     }
 }
